cea13e025880a32d2d06d3dd9d71a5467ad86381
jaynamm <polky0421@gmail.com>
2023-03-22 12:16:50 +0900

    init java project

---
+++ src/sec01/exam01/VariableExchangeExample.java
@@ -0,0 +1,10 @@
+package sec01.exam01;
+
+public class VariableExchangeExample {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 5;
+
+        System.out.println("x = " + x + " / y = " + y);
+    }
+}
